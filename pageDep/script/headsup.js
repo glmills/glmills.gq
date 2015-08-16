7b0d0dc00b2b10da0d3174c4a4590c2b9ba15576
glmills1 <garrett@glmills.gq>
2015-08-16 16:07:58 +0000

    added jsmobile function

--- pageDep/script/headsup.js
+++ pageDep/script/headsup.js
@@ -1,12 +1,13 @@
 /* Coded by Garrett Mills 
  * http://glmills.gq/ 
- * Last Edited on 5 August 13:18
+ * Last Edited on 16 August 11:00
  * This code is licensed under Creative Commons Attribution-ShareAlike 4.0 International License
  * http://glmills.gq/mycode/licenses/CCA-SA4.html 
  */
 document.write('\
 \
     <script src="/lib/jsurl/src/getvars.js"></script>\
+    <script src="/lib/jsmobile/src/check.js"></script>\
     <title>Garrett Mills</title>\
     <script src="/pageDep/script/pageNavi.js"></script>\
     <script src="/pageDep/script/socialNavi.js"></script>\
